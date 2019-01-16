c79fad9dd73c4c3cc05bc787c26ee6fa174c60ab
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-16 19:06:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,6 +7,9 @@ class Song
     end
 
     def self.new_by_filename(filename)
+        stripped = file_name.split(" - ")
+        name = stripped[1]
+        artist = stripped[0]
         song = Song.new(filename)
         song.artist = artist
     end

e4a4667156d81d9baf1df3e7c6216775ba097741
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-16 17:17:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,8 +7,6 @@ class Song
     end
 
     def self.new_by_filename(filename)
-        artist = filename.split(" - ")[0]
-        title =
         song = Song.new(filename)
         song.artist = artist
     end
